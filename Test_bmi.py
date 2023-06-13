bd6c0307fa19367c74df5386c7f8c32828d553fb
zhiyu <lzhiyuu7@gmail.com>
2023-06-13 16:03:35 +0800

    commiting Test_bmi.py

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,1 +1,22 @@
-import Lab3.ex1_prac(cloned) as bmi
+import lab2.ex1_prac as bmi
+
+def Test_bmi_normal_weight():
+    input_weight = 51
+    result = bmi.calculate_bmi(1.59, input_weight)
+    test = 0
+
+    assert(result == test)
+
+def Test_bmi_over_weight():
+    input_weight = 80
+    result = bmi.calculate_bmi(1.59, input_weight)
+    test = 1
+
+    assert(result == test)
+
+def Test_bmi_under_weight():
+    input_weight = 40
+    result = bmi.calculate_bmi(1.59, input_weight)
+    test = -1
+
+    assert(result == test)
